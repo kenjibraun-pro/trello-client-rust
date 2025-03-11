7e8237679334266b34e21be2b485b3243c9eb7f7
Kenji Braun <kenjibraun.pro@gmail.com>
2025-03-10 22:36:48 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ use tc::commands::{
 #[tokio::main(flavor = "multi_thread", worker_threads = 8)]
 async fn main() {
     dotenv().ok();
-
+    
     let cli = Cli::parse();      
 
     match &cli.command {
         Commands::Add(add) => add_commands(add).await,
         Commands::Get(get) => get_commands(get).await,
-        Commands::Move(mov) => move_commands(mov),
+        Commands::Move(mov) => move_commands(mov),    
         Commands::Edit(edit) => edit_commands(edit).await,
-        Commands::Remove(remove) => remove_commands(remove).await,
+        Commands::Remove(remove) => remove_commands(remove).await,    
     }
 }
